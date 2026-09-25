aeca5f89c76be46a6fae2f8d4803b2e9784a26eb
Christian Petrov <christian.petrov.1@proton.me>
2026-09-26 00:02:27 +0300

    Controller: New ProjectController with Index and Details methods.
    Index: Return a list of available Projects and Materials from DB as a list. This acts as a home page for uploading new Assets. User can specify if they want to create a new Project with an Asset or insert an Asset in an already existing Project.
    
    Details:  Returns a view showing details about an Asset based on input Id.

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,59 @@
+﻿
+namespace PartManagementSystem.Controllers
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+
+    using PartManagementSystem.Data;
+    using PartManagementSystem.ViewModels;
+
+    public class ProjectController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public ProjectController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() // This returns a list of available Projects and Materials from DB.
+        {
+            var viewModel = new AssetUploadViewModel
+            {
+                Projects = await dbContext.Projects
+                .Select(p => new SelectListItem { Value = p.ProjectId.ToString(), Text = p.ProjectName})
+                .ToListAsync(),
+
+                Materials = await dbContext.Materials
+                .Select(m => new SelectListItem { Value = m.MaterialId.ToString(), Text = m.MaterialName})
+                .ToListAsync()
+            };
+
+            return View(viewModel);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+
+            var asset = await dbContext.Assets
+                .Include(a => a.Project)
+                .Include(a => a.Material)
+                .Include(a => a.Owner)
+                .Include(a => a.CurrentRevision)
+                .Include(a => a.Revisions)
+                .FirstOrDefaultAsync(a => a.AssetId == id && !a.IsDeleted);
+
+            if (asset == null)
+            {
+                return NotFound();
+            }
+
+            return View(asset);
+        }
+    }
+}
